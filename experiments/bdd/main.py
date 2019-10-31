2bf763380eed4c7ddf4d9e57ca34df31ef7706ad
CaptainTrunky <captaintrunky@gmail.com>
2019-10-31 16:00:55 +0300

    Update main.py

--- experiments/bdd/main.py
+++ experiments/bdd/main.py
@@ -18,6 +18,9 @@ from metrics.generic import get_all_metrics
 
 from models.bdd.SemSeg import SemSeg as Model
 
+from thirdparty.ranger.ranger import Ranger
+import thirdparty.LovaszSoftmax.pytorch.lovasz_losses as L
+
 from utils.vis import get_colormap
 
 from sacred import Experiment
@@ -35,33 +38,42 @@ ex.observers.append(MongoObserver.create())
 writer = None
 
 # ignore, moving car, parked car, person, semaphore, road
+#VALID_MASK_IDS = {
+#    0: 'road',
+#    1: 'sidewalk',
+#    6: 'semaphore',
+#    7: 'sign',
+#    11: 'person',
+#    13: 'car',
+#    14: 'truck',
+#    15: 'bus'
+#}
+
 VALID_MASK_IDS = {
     0: 'road',
     1: 'sidewalk',
-    6: 'semaphore',
-    11: 'person',
-    13: 'car',
-    15: 'pickup'
+    2: 'semaphore',
+    3: 'sign',
+    4: 'person',
+    5: 'car'
 }
-
-EARLY_STOP = 2
 
 
 @ex.config
-def ex_config():
-    root_path = Path('/home/sbykov/workspace/datasets')
-
-    trainer_config = {
-        'dataset_path': root_path / 'bdd100k' / 'seg',
-        'device': 'cuda',
-        'num_classes': 20,
-        'height': 256,
-        'width': 256,
-        'epochs': 10,
-        'learning_rate': 1e-4,
-        'batch_size': 16,
-        'shuffle': True
-    }
+def default():
+    trainer_config = { 
+        "dataset_path": "./processed",
+        "device": "cuda",
+        "num_classes": 8,
+        "height": 256,
+        "width": 256,
+        "epochs": 10,
+        "loss": "lovasz_softmax",
+        "resize_strategy": "crop",
+        "learning_rate": 1e-3,
+        "batch_size": 16,
+        "shuffle": True
+     }
 
 
 def train(config, loaders):
@@ -72,13 +84,21 @@ def train(config, loaders):
 
     model = Model(config.num_classes)
 
-    optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
+    optimizer = Ranger(model.parameters(), lr=config.learning_rate)
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min')
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
     model.to(config.device)
 
-    criterion = T.nn.CrossEntropyLoss(ignore_index=255).to(config.device)
+    #weights = T.ones(size=(config.num_classes,)).float()
+
+    #for k, v in config.weights.items():
+    #    weights[k] = v
+
+    criterion = None
+
+    if config.loss == 'wce':
+        criterion = T.nn.CrossEntropyLoss(weight=weights.to(config.device), ignore_index=255).to(config.device)
 
     logger = config.run
 
@@ -94,7 +114,9 @@ def train(config, loaders):
         train_loss = train_epoch(model, optimizer, criterion, train_loader, epoch, logger)
         val_loss = val_step(model, val_loader, criterion, epoch, logger)
 
-        scheduler.step()
+        scheduler.step() 
+
+        logger.log_scalar('train.lr', scheduler.optimizer.param_groups[0]['lr'])
 
         if epoch % 5 == 0:
             model_name = f"./best_model_{epoch}_{str(val_loss).replace('.', 'd')}.pth"
@@ -127,7 +149,7 @@ def train_epoch(model, optimizer, criterion, train_loader, epoch, logger):
 
         predict = model(data)['out']
 
-        loss = criterion(input=predict, target=labels)
+        loss = L.lovasz_softmax(probas=predict, labels=labels, ignore=255)
 
         with T.no_grad():
             losses.append(loss.mean().item())
@@ -137,9 +159,6 @@ def train_epoch(model, optimizer, criterion, train_loader, epoch, logger):
         loss.backward()
 
         optimizer.step()
-
-        if idx > EARLY_STOP:
-            break
 
     if writer:
         writer.add_scalar('train.cross_entropy', np.mean(losses), global_step=epoch)
@@ -166,7 +185,7 @@ def val_step(model, val_loader, criterion, epoch, logger):
 
             predict = model(data)['out']
 
-            loss = criterion(input=predict, target=labels)
+            loss = L.lovasz_softmax(probas=predict, labels=labels, ignore=255)
 
             losses.append(loss.mean().item())
 
@@ -180,27 +199,35 @@ def val_step(model, val_loader, criterion, epoch, logger):
                         label_id
                     )
 
-                    metric_name = f'{name}_{label_name}'
+                    metric_name = f'{label_name}_{name}'
 
                     if metric_name not in metrics_values:
                         metrics_values[metric_name] = [val]
                     else:
                         metrics_values[metric_name].append(val)
 
-            if idx > EARLY_STOP:
-                break
-
     if writer:
         writer.add_scalar('val.accuracy', 100 * acc / total_samples, global_step=epoch)
     else:
         logger.log_scalar('val.cross_entropy', np.mean(losses))
 
+        ious = []
         for metric_name, vals in metrics_values.items():
-            logger.log_scalar(f'test.{metric_name}', np.mean(val))
+            logger.log_scalar(f'test.{metric_name}', np.mean(vals))
+  
+            if '_iou' in metric_name:
+                m = np.mean(vals)
+                logging.info(f'test.{metric_name}: {m}')
+
+                ious.append(m)
+
+        mean_ious = np.mean(ious)
+        logging.info(f'test.mean_iou: {mean_ious}')
+        logger.log_scalar('test.mean_iou', mean_ious)
 
         imgs = masks.detach().cpu().numpy()
 
-        colormap = get_colormap()
+        colormap = get_colormap(len(VALID_MASK_IDS))
         # dump colored images
         for i in range(3):
             img = imgs[i, :, :]
@@ -223,10 +250,10 @@ def ex_main(_run, trainer_config):
     T.backends.cudnn.deterministic = True
     T.backends.cudnn.benchmark = False
 
-    trainer_config['run'] = _run
-
     from munch import Munch
     trainer_config = Munch(trainer_config)
+
+    trainer_config.run = _run
 
     loaders = init_dataloaders(config=trainer_config)
 
